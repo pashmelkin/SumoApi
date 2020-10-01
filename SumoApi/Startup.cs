fb4796b1a83964e6d2f451a478efb91e0ba5442e
Pavel <pashmelkin@gmail.com>
2020-10-01 11:14:49 +1000

    Add cors (#6)

--- SumoApi/Startup.cs
+++ SumoApi/Startup.cs
@@ -16,6 +16,7 @@ namespace Deployment
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +27,14 @@ namespace Deployment
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("http://localhost:3000");
+                                  });
+            });
             services.AddControllers();
             services.AddScoped<ISumoQueryService, SumoQueryService>();
             services.AddScoped<IDeploymentService, DeploymentService>();
@@ -83,6 +92,7 @@ namespace Deployment
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseAuthorization();
 
